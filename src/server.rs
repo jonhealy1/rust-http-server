b009211f4c3f0ba694c6a8440378993db0d3f9be
jonhealy1 <jonathan.d.healy@gmail.com>
2021-02-01 21:51:40 -0800

    finish course

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,16 @@
-use std::net::TcpListener;
+use crate::http::{ParseError, Request, Response, StatusCode};
+use std::convert::TryFrom;
 use std::io::Read;
+use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 
 pub struct Server {
     addr: String,
@@ -10,7 +21,7 @@ impl Server {
         Self { addr }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
@@ -22,12 +33,21 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                        },
-                        Err(e) => println!("Failed to read from connection {}", e),
+
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
                     }
-                },
-                Err(e) => println!("Failed to establish connection: {}", e),
+                }
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
         }
-    } 
+    }
 }
